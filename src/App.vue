41f4323ee8efd6289a15876742b0d74a98c3af71
gavin@sfshf <gavin@github.com>
2022-06-15 14:33:07 +0800

    pug & axios packages.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
-<template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<template lang="pug">
+nav
+  router-link(to="/") Home |
+  router-link(to="/about") About
+router-view
 </template>
 
 <style lang="scss">
